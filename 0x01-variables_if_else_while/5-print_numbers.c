329708ddb5239377a7648ef1035314b50380c05a
Mofeyisara <mofeyisaraajao@gmail.com>
2023-06-18 11:17:01 -0700

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	int n:
+	int i:
 
-	for (n = 0; n < 10; n++)
+	for (i = 0; i < 10; i++)
 	{
-		printf("%d", n);
+		printf("%d", i);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
