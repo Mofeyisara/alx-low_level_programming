33e0f294ca3f7f6b02cd5180807de6c38b7065a4
Mofeyisara <mofeyisaraajao@gmail.com>
2023-06-18 09:38:17 -0700

    Print Aphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: 0 (Success)
@@ -7,12 +6,18 @@
 
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	int n = 97;
+	int m = 65;
 
-	for (i = 0; i < 52; i++)
+	while (n <= 122)
 	{
-		putchar(alp[i]);
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
 	}
 	putchar('\n');
 	return (0);
