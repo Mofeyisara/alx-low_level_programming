1d368b2a2c749218dce42e499e681f787f6a0079
Mofeyisara <mofeyisaraajao@gmail.com>
2023-06-18 10:45:16 -0700

    Print letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * main - Entry point
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 	int l = 'a';
 
 	while (l <= 'z')
 	{
-	if (l == 'e' || l == 'q')
-	{
-		l += 1;
+		if (l == 'e' || l == 'q')
+		{
+			l += 1;
+		}
 	}
 	else
 	{
