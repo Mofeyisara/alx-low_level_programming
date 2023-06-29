b782932d00d14f13f83769598946f7ce92d0c897
Mofeyisara <mofeyisaraajao@gmail.com>
2023-06-29 11:25:47 -0700

    Strings and arrays

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,14 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar - Write char c to stdout
  * @c: char to print
+ *
  * Return: 1 on success
+ * Return -1 on error
  */
 int _putchar(char c)
 {
-	return(write(1, @c, 1));
+	return(write(1, &c, 1));
 }
