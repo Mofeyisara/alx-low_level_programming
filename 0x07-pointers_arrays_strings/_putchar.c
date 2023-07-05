d3290ba0c25016f036e2de4381b736007ff6fb75
Mofeyisara <mofeyisaraajao@gmail.com>
2023-07-05 08:40:27 -0700

    0x07 putchar

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/***
+ * _putchar - write the char to stdout
+ * @c: char to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
