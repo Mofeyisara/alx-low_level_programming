27afa255c5dd215eb3166658b51532b87351a78f
Mofeyisara <mofeyisaraajao@gmail.com>
2023-07-08 06:54:58 -0700

    My name is

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the c char to stdout
+ *
+ * @c: char to print
+ * Return: 1 or -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
