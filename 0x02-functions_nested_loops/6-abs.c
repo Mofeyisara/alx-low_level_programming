6ed5efc61c50934f55baf91d463ee1c3721129b6
Mofeyisara <mofeyisaraajao@gmail.com>
2023-07-09 03:50:54 -0700

    Functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Compute the absolute value
+ * @i: Input
+ *
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
